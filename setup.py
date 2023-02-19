a2116aefd4fad916a2aa63b6c74b3f33597d2aec
Takuma Mori <takuma104@gmail.com>
2023-02-19 22:23:36 +0900

    move all source code to src

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -8,17 +9,18 @@ def _requires_from_file(filename):
 
 setuptools.setup(
     name="controlnet_hinter",
-    version="0.0.1",
+    version="0.0.2",
     author="takuma104",
     author_email="takuma104@gmail.com",
     description="ControlNet hint embedding utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/takuma104/controlnet_hinter",
-    py_modules=['controlnet_hinter', 'annotator'],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     install_requires=_requires_from_file('requirements.txt'),
